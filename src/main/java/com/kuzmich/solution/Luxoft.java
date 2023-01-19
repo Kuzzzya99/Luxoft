0515b9f603554c4c440a31ae88328f10e1d3f8ee
Kuzzzya99 <kuzaka247872@gmail.com>
2023-01-19 15:57:31 +0300

    Initial commit

---
+++ src/main/java/com/kuzmich/solution/Luxoft.java
@@ -0,0 +1,9 @@
+package com.kuzmich.solution;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Luxoft {
+    public static void main(String... args) {
+        SpringApplication.run(Luxoft.class, args);
+    }
+}
